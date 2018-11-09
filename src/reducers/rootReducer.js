8c61a59d838cce2be06a300c42f071141080a313
Võ Minh Trí <vominhtrius@gmail.com>
2018-11-09 22:12:00 +0700

    delete some folder

--- src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { firebaseReducer } from 'react-redux-firebase'
-import {combineReducers} from 'redux';
-import chatReducer from './chatReducer';
-
-export default combineReducers({
-    firebase: firebaseReducer,
-    chatReducer
-  });
