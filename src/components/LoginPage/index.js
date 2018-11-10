fe72935966fd67f83cd6772ec886fd28b924954e
Võ Minh Trí <vominhtrius@gmail.com>
2018-11-10 19:59:05 +0700

    add loader for login page

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Loader from '../Loader';
+
 import './Login.css';
+import { isLoaded } from 'react-redux-firebase';
 
 class LoginPage extends Component {
     onClickLogin = () => {
@@ -10,23 +13,29 @@ class LoginPage extends Component {
     }
 
     render() {
+        const _isLoaded = isLoaded(this.props.auth);
+
         return (
             <div className="login-container">
                 <div className="wrap-login">
                     <div className="login-form">
                         <span className="login-form-title"> Chat App - 1512607 </span>
-                        <button
-                            className="btn-google"
-                            onClick={() => this.onClickLogin()}
-                        >
-                            <span className="btn-google-icon-wrapper">
-                                <img className="btn-google-icon" alt="" src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg">
-                                </img>
+                        {
+                            !_isLoaded ?
+                                <Loader /> :
+                                <button
+                                    className="btn-google"
+                                    onClick={() => this.onClickLogin()}
+                                >
+                                    <span className="btn-google-icon-wrapper">
+                                        <img className="btn-google-icon" alt="" src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg">
+                                        </img>
+                                    </span>
+                                    <span className="btn-google-text">
+                                        Login with Google
                             </span>
-                            <span className="btn-google-text">
-                                Login with Google
-                            </span>
-                        </button>
+                                </button>
+                        }
                     </div>
                 </div>
             </div>
